870b190e1c274709de53e107f511f9b570742143
papparazzo <pappi-@gmx.de>
2020-06-29 21:07:04 +0200

    XSRF-Token class added

--- CHANGE.md
+++ CHANGE.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.1.0] - 2020-06-29
+### Added
+- XSRFToken class added
+
+
 ## [2.0.0] - 2020-06-28
 ### Added
 - Interface and abstract class added
---
+++ src/XSRFToken.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ *  Project:    sfw2-session
+ *
+ *  Copyright (C) 2020 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+namespace SFW2\Session;
+
+class XSRFToken {
+
+    protected SessionInterface $session;
+
+    public function __construct(SessionInterface $session) {
+        $this->session = $session;
+    }
+
+    public function generateToken(): string {
+        $token = md5(random_int(PHP_INT_MIN, PHP_INT_MAX) . uniqid("", true));
+        $this->session->setGlobalEntry(SessionInterface::XSS_TOKEN, $token);
+        return $token;
+    }
+
+    public function compareToken(string $rtoken): bool {
+        $token = $this->session->getGlobalEntry(SessionInterface::XSS_TOKEN);
+        if($token == null) {
+            return false;
+        }
+        $this->session->delGlobalEntry(SessionInterface::XSS_TOKEN);
+        return ($rtoken == $token);
+    }
+
+    public function getToken(): string {
+        $token = $this->session->getGlobalEntry(SessionInterface::XSS_TOKEN);
+        $this->session->delGlobalEntry(self::XSS_TOKEN);
+        return $token;
+    }
+}
